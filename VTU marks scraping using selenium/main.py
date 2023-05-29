7133ab4b8ad5a92554f593512f9c729f23bdd694
nithinhm <nithinmanju111@gmail.com>
2023-05-29 20:59:42 +0530

    updated main.py

--- VTU marks scraping using selenium/main.py
+++ VTU marks scraping using selenium/main.py
@@ -1,22 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions
 import pandas as pd
 import numpy as np
 
 
+options = Options()
+options.unhandled_prompt_behavior = 'ignore'
+
 batch = '21'        # enter batch year as in USN (4th and 5th character)
-branch = 'MT'       # enter branch as in USN (6th and 7th character)
-final_number = 13   # enter the last number of this branch's last student as in USN (do not include padded zeros)
+branch = 'ME'       # enter branch as in USN (6th and 7th character)
+final_number = 6   # enter the last number of this branch's last student as in USN (do not include padded zeros)
 
 df = pd.DataFrame()
 
-driver = webdriver.Chrome('chromedriver.exe')
+driver = webdriver.Chrome('chromedriver.exe', options=options)
 
 url = 'https://results.vtu.ac.in/JFEcbcs23/index.php'
 
 driver.get(url)
+
+skipped_usns = []
 
 k = 1
 
@@ -26,34 +32,31 @@ while k <= final_number:
     usn_bar = driver.find_element(By.NAME ,'lns')
     usn_bar.send_keys(usn)
 
-    captcha_bar = driver.find_element(By.XPATH ,'//*[@id="raj"]/div[2]/div[1]/label')
+    captcha_bar = driver.find_element(By.NAME ,'captchacode')
     captcha_bar.click()
 
     try:
-        WebDriverWait(driver, timeout=100).until(lambda d: d.find_element(By.XPATH ,
-            '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[3]/div[3]/table/tbody/tr[2]/td/b[2]'))
+        WebDriverWait(driver, 100).until_not(lambda d: d.find_element(By.NAME ,'lns'))
+
     except:
-        alert = WebDriverWait(driver, timeout=5).until(expected_conditions.alert_is_present())
-
-        if alert.text == 'Invalid captcha code !!!':
-            alert.accept()
-            continue
-        elif alert.text == 'University Seat Number is not available or Invalid..!':
+        alert = WebDriverWait(driver, 1).until(expected_conditions.alert_is_present())
+        print(f'Error for {usn} because {alert.text}')
+        
+        if alert.text == 'University Seat Number is not available or Invalid..!':
             k += 1
+            skipped_usns.append(usn)
+            alert.accept()
+            continue
+        else:
             alert.accept()
             continue
 
-    student_name = driver.find_element(By.XPATH,
-        '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[2]/td[2]').text.split(':')[
-        1].strip()
-    student_usn = driver.find_element(By.XPATH,
-        '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[1]/td[2]').text.split(':')[
-        1].strip()
+    student_name = driver.find_element(By.XPATH, '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[2]/td[2]').text.split(':')[1].strip()
+    student_usn = driver.find_element(By.XPATH, '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[1]/td[2]').text.split(':')[1].strip()
 
     assert student_usn == usn, 'entered usn and obtained usn don\'t match'
 
-    marks_data = driver.find_element(By.XPATH,
-        '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]')
+    marks_data = driver.find_element(By.XPATH, '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]')
 
     data_array = np.zeros(shape=(9, 7), dtype=object)
 
@@ -76,11 +79,15 @@ while k <= final_number:
 
     df[student_usn] = np.concatenate((np.array([student_name]), data_array[1:, 2:].flatten()))
 
+    print(f'Data successsfully collected for {usn}')
+
     driver.back()
 
     k += 1
 
-driver.quit()
+# driver.quit()
+
+print(f'These usns were skipped {skipped_usns}')
 
 df2 = df.T.reset_index()
 df2.columns = [('USN',''),('Student Name','')] + list_of_tuples
