2e50edb9a85b3282c48293729377eaba34ae24d9
nithinhm <nithinmanju111@gmail.com>
2023-05-30 12:14:32 +0530

    captcha tag update

--- VTU marks scraping using selenium/main.py
+++ VTU marks scraping using selenium/main.py
@@ -6,13 +6,12 @@ from selenium.webdriver.support import expected_conditions
 import pandas as pd
 import numpy as np
 
-
 options = Options()
 options.unhandled_prompt_behavior = 'ignore'
 
-batch = '21'        # enter batch year as in USN (4th and 5th character)
-branch = 'ME'       # enter branch as in USN (6th and 7th character)
-final_number = 6   # enter the last number of this branch's last student as in USN (do not include padded zeros)
+batch = '22'  # enter batch year as in USN (4th and 5th character)
+branch = 'CS'  # enter branch as in USN (6th and 7th character)
+final_number = 232  # enter the last number of this branch's last student as in USN (do not include padded zeros)
 
 df = pd.DataFrame()
 
@@ -24,24 +23,24 @@ driver.get(url)
 
 skipped_usns = []
 
-k = 1
+k = 101
 
 while k <= final_number:
     usn = '1AM' + batch + branch + f'{k}'.zfill(3)
 
-    usn_bar = driver.find_element(By.NAME ,'lns')
+    usn_bar = driver.find_element(By.NAME, 'lns')
     usn_bar.send_keys(usn)
 
-    captcha_bar = driver.find_element(By.NAME ,'captchacode')
+    captcha_bar = driver.find_element(By.XPATH, '//*[@id="raj"]/div[2]/div[1]/label')
     captcha_bar.click()
 
     try:
-        WebDriverWait(driver, 100).until_not(lambda d: d.find_element(By.NAME ,'lns'))
+        WebDriverWait(driver, 100).until_not(lambda d: d.find_element(By.NAME, 'lns'))
 
     except:
         alert = WebDriverWait(driver, 1).until(expected_conditions.alert_is_present())
         print(f'Error for {usn} because {alert.text}')
-        
+
         if alert.text == 'University Seat Number is not available or Invalid..!':
             k += 1
             skipped_usns.append(usn)
@@ -51,12 +50,17 @@ while k <= final_number:
             alert.accept()
             continue
 
-    student_name = driver.find_element(By.XPATH, '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[2]/td[2]').text.split(':')[1].strip()
-    student_usn = driver.find_element(By.XPATH, '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[1]/td[2]').text.split(':')[1].strip()
+    student_name = driver.find_element(By.XPATH,
+                                       '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[2]/td[2]').text.split(
+        ':')[1].strip()
+    student_usn = driver.find_element(By.XPATH,
+                                      '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr[1]/td[2]').text.split(
+        ':')[1].strip()
 
     assert student_usn == usn, 'entered usn and obtained usn don\'t match'
 
-    marks_data = driver.find_element(By.XPATH, '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]')
+    marks_data = driver.find_element(By.XPATH,
+                                     '//*[@id="dataPrint"]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]')
 
     data_array = np.zeros(shape=(9, 7), dtype=object)
 
@@ -67,7 +71,7 @@ while k <= final_number:
 
     data_array = data_array[:, :-1]
 
-    if k == 1:
+    if k == 101:
         header_names = data_array[0][2:]
         subject_names = data_array[:, 1][1:]
 
@@ -90,10 +94,10 @@ while k <= final_number:
 print(f'These usns were skipped {skipped_usns}')
 
 df2 = df.T.reset_index()
-df2.columns = [('USN',''),('Student Name','')] + list_of_tuples
-df2.columns = pd.MultiIndex.from_tuples(df2.columns, names=['',''])
-df2.index+=1
+df2.columns = [('USN', ''), ('Student Name', '')] + list_of_tuples
+df2.columns = pd.MultiIndex.from_tuples(df2.columns, names=['', ''])
+df2.index += 1
 
 df2 = df2.apply(pd.to_numeric, errors='ignore')
 
-df2.to_excel(f'20{batch} {branch} VTU results.xlsx')
+df2.to_excel(f'20{batch} {branch} PART-2 VTU results.xlsx')
