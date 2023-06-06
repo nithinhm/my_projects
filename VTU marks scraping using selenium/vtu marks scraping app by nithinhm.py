25138debdab41eb4c4479cfce3c93c89d10231d0
nithinhm <nithinmanju111@gmail.com>
2023-06-06 16:41:39 +0530

    visual updates

--- VTU marks scraping using selenium/vtu marks scraping app by nithinhm.py
+++ VTU marks scraping using selenium/vtu marks scraping app by nithinhm.py
@@ -13,37 +13,72 @@ import time
 skipped_usns = []
 data_dict = {}
 
+
+service = ChromeService(executable_path='/chromedriver.exe')
 options = Options()
 options.unhandled_prompt_behavior = 'ignore'
 
-print('Example USN: 1AM22CS010\n')
+art = '''
+   *                       (                                                                     (                         
+ (  `               )      )\ )                              (                      *   )        )\ )            (    )    
+ )\))(     ) (   ( /(     (()/(    (      )         (  (     )\ )     (     (   ( ` )  /(   (   (()/(  (      (  )\( /(    
+((_)()\ ( /( )(  )\())(    /(_)) ( )(  ( /( `  )   ))\ )(   (()/(  (  )(    )\  )\ ( )(_))  )\   /(_))))\(   ))\((_)\()|   
+(_()((_))(_)|()\((_)\ )\  (_))   )(()\ )(_))/(/(  /((_|()\   /(_)) )\(()\  ((_)((_|_(_())_ ((_) (_)) /((_)\ /((_)_(_))/)\  
+|  \/  ((_)_ ((_) |(_|(_) / __| ((_|(_|(_)_((_)_\(_))  ((_) (_) _|((_)((_) \ \ / /|_   _| | | | | _ (_))((_|_))(| | |_((_) 
+| |\/| / _` | '_| / /(_-< \__ \/ _| '_/ _` | '_ \) -_)| '_|  |  _/ _ \ '_|  \ V /   | | | |_| | |   / -_|_-< || | |  _(_-< 
+|_|  |_\__,_|_| |_\_\/__/ |___/\__|_| \__,_| .__/\___||_|    |_| \___/_|     \_/    |_|  \___/  |_|_\___/__/\_,_|_|\__/__/ 
+                                           |_|                                                                             
+'''
+
+print(art)
+
+print("\nWelcome to the VTU marks scraping app developed by Prof. Nithin H M, Assistant Professor, AMC Engineering College.")
+print("\nProcedure:\n1. Fill the details as mentioned below.\n2. When the browser opens, wait till the USN is auto-filled, enter the CAPTCHA and hit enter.\n3. The marks data is collected for that student and the process repeats.\n4. You can keep an eye on this console to check for status updates or errors that occur.")
+
+print('\nExample USN: 1AM22CS010')
+
 while True:
     try:
-        batch = int(input('Enter batch year as in USN (Ex: 22 or 21 or 19): ').strip())
+        coll_code = input('\nEnter your college code as in USN (Ex: 1AM): ').upper().strip()
     except:
-        print('Error! Enter a valid 2-digit number.\n')
+        print('Error! Enter a valid 3-character code.')
     else:
-        batch = str(batch)
-        if len(batch) != 2:
-            print('Error! Enter a valid 2-digit number.\n')
+        if len(coll_code) != 3 or (not isinstance(int(coll_code[0]), int)) or (not isinstance(coll_code[1:], str)):
+            print('Error! Enter a valid 3-character code.')
         else:
             break
 
 while True:
-    branch = input('\nEnter branch as in USN (Ex: CS or MT or CI): ').upper().strip()
-    if len(branch) != 2:
-        print('Error! Enter a valid 2-character branch.\n')
+    try:
+        batch = int(input('\nEnter batch year as in USN (Ex: 22 or 21 or 19): ').strip())
+    except:
+        print('Error! Enter a valid 2-digit number.')
     else:
-        break
+        batch = str(batch)
+        if len(batch) != 2:
+            print('Error! Enter a valid 2-digit number.')
+        else:
+            break
+
+while True:
+    try:
+        branch = input('\nEnter branch as in USN (Ex: CS or MT or CI): ').upper().strip()
+    except:
+        print('Error! Enter a valid 2-character branch.')
+    else:
+        if len(branch) != 2:
+            print('Error! Enter a valid 2-character branch.')
+        else:
+            break
 
 while True:
     try:
         first_number = int(input("\nEnter the first number of this branch's USN (Ex: 1 or 25 or 140): ").strip())
     except:
-        print('Error! Enter a valid 3-digit number.\n')
+        print('Error! Enter a valid 3-digit number.')
     else:
         if first_number > 999:
-            print('Error! Enter a valid 3-digit number.\n')
+            print('Error! Enter a valid 3-digit number.')
         else:
             break
 
@@ -51,10 +86,10 @@ while True:
     try:
         final_number = int(input("\nEnter the last number of this branch's USN (Ex: 5 or 23 or 145): ").strip())
     except:
-        print('Error! Enter a valid 3-digit number.\n')
+        print('Error! Enter a valid 3-digit number.')
     else:
         if final_number > 999:
-            print('Error! Enter a valid 3-digit number.\n')
+            print('Error! Enter a valid 3-digit number.')
         else:
             break
 
@@ -62,7 +97,7 @@ while True:
     try:
         retry_delay = int(input('\nIf there are network issues, how many seconds do you wish to wait before retrying again?: ').strip())
     except:
-        print('Error! Enter a valid number.\n')
+        print('Error! Enter a valid number.')
     else:
         break
 
@@ -70,13 +105,21 @@ while True:
     try:
         max_retries = int(input('\nHow many times do you wish to retry before ending the session?: ').strip())
     except:
-        print('Error! Enter a valid number.\n')
+        print('Error! Enter a valid number.')
     else:
         break
 
-url = input('\nEnter the URL of the results login page. (starts with https://): ').strip()
+while True:
+    try:
+        url = input('\nEnter the URL of the results login page (which contains fields for entering USN and CAPTCHA; it should start with https://): ').strip()
+    except:
+        print('Error! Enter a valid url.')
+    else:
+        if 'https://' not in url:
+            print("Error! Please include https:// in the url.")
+        else:
+            break
 
-service = ChromeService(executable_path='/chromedriver.exe')
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get(url)
@@ -85,13 +128,15 @@ k = first_number - 1
 
 window_is_present = True
 
+print("\n")
+
 while k < final_number and window_is_present:
     
     k += 1
     this_retry = 0
     while this_retry < max_retries:
         try:
-            usn = f'1AM{batch}{branch}{k:03d}'
+            usn = f'{coll_code}{batch}{branch}{k:03d}'
 
             usn_bar = driver.find_element(By.NAME, 'lns')
             usn_bar.send_keys(usn)
@@ -181,9 +226,9 @@ while k < final_number and window_is_present:
 driver.quit()
 
 if len(skipped_usns) > 0:
-    print(f'These USNs were skipped {skipped_usns}.\n')
+    print(f'\nThese USNs were skipped {skipped_usns}.')
 else:
-    print('No USNs were skipped.\n')
+    print('\nNo USNs were skipped.')
 
 list_of_student_dfs = []
 
@@ -225,6 +270,6 @@ first_USN, last_USN = collected_usns[0], collected_usns[-1]
 
 df2.to_excel(f'20{batch} {branch} {first_USN} to {last_USN} VTU results.xlsx')
 
-print(f'Data collected for USNs {branch} {first_USN} to {last_USN} and saved in an excel file.\n')
+print(f'\nData collected for USNs {branch} {first_USN} to {last_USN} and saved in an excel file.\n')
 
-input("Press any button to close this app.")
+input("Press enter to close this app.")
